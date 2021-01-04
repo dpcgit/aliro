4fead9912b09a624ab2dd1ee890b06189e622f49
Daniel Puentes Camacho <daniel.puentes.ca@tutanota.com>
2021-01-04 15:55:03 -0600

    added two poiint logic

--- src/App.js
+++ src/App.js
@@ -29,6 +29,19 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import './App.css'
 
+// code to fix rendering leaflet marker icon
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+// end of icon rendering code fix
+
+
 function App() {
   const mapRef = useRef(null);
 
@@ -36,6 +49,11 @@ function App() {
   const [results,setResults] = useState([]);
   const [selected,setSelected] = useState([49.8419, 24.0315])
   
+
+  const [input2,setInput2] = useState('');
+  const [results2,setResults2] = useState([]);
+  const [selected2,setSelected2] = useState([49.8419, 24.0315])
+
   const defaultZoom = 8;
   const styleMap = { "width": "75vw", "height": "75vh" };
 
@@ -43,8 +61,14 @@ function App() {
     console.log(e.target.id)    
     await setSelected(e.target.id.split("_").map((el)=>parseFloat(el)))
     console.log(selected)
-    console.log('setview')
-    
+    console.log('setview')    
+  }
+
+  async function handlePlaceClick2(e){
+    console.log(e.target.id)    
+    await setSelected2(e.target.id.split("_").map((el)=>parseFloat(el)))
+    console.log(selected2)
+    console.log('setview')    
   }
 
   async function handleInputChange(e){
@@ -56,6 +80,17 @@ function App() {
     console.log(data);
     
     setResults(data);
+  }
+
+  async function handleInputChange2(e){
+    setInput2(e.target.value);
+    
+    const api_url="http://nominatim.openstreetmap.org/search?format=json&limit=5&q=" + e.target.value;
+    const response = await fetch(api_url);   
+    const data = await response.json();
+    console.log(data);
+    
+    setResults2(data);
   }
   
   useEffect(() => {
@@ -74,19 +109,36 @@ function App() {
 
   useEffect(() => {
     // add selected location logic
+    L.marker(selected).addTo(mapRef.current);
     mapRef.current.setView(selected);
   }, [selected]);
+
+
+  useEffect(() => {
+    // add selected location logic
+    L.marker(selected2).addTo(mapRef.current);
+    mapRef.current.setView(selected2);
+  }, [selected2]);
 
   return(
     <div>
       From
       <input
-            value={input}
-            onChange={handleInputChange} />          
+        value={input}
+        onChange={handleInputChange}
+      />
       <div>
           {results.map((el,i)=>(<a href="#" id={el.lat+"_"+el.lon} onClick={handlePlaceClick}>{el.display_name}<br/></a>))}
       </div>
-
+      To         
+      <input
+        value={input2}
+        onChange={handleInputChange2}
+      />
+      <div>
+          {results2.map((el,i)=>(<a href="#" id={el.lat+"_"+el.lon} onClick={handlePlaceClick2}>{el.display_name}<br/></a>))}
+      </div>
+      Map
       <div id="map">        
       </div>
     </div>
